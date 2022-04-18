2fdaacdd4a044ae591d0a633999f8b34b1ce8dca
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-18 12:35:00 -0300

    ✨ add unauthenticated and 404 page

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,18 +1,28 @@
+import {UseSessionOptions} from 'next-auth/react';
 import {useEffect, useState} from 'react';
 
 type Props = {
   service: (param?: any) => Promise<any>;
+  status?: 'authenticated' | 'unauthenticated' | 'loading';
 };
 
-export default function useFetch<T>({service}: Props) {
+export default function useFetch<T>({service, status}: Props) {
   const [data, setData] = useState<T | undefined>(undefined);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    service().then((data) => {
-      setData(data);
-      setLoading(false);
-    });
-  }, []);
+    if (status === 'authenticated') {
+      service().then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+    }
+    if (!status) {
+      service().then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+    }
+  }, [status]);
   return {data, isLoading};
 }
