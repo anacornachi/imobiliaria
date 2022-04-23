18285edbd2592bf62689a38da493c60249bdaf6f
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-23 16:48:28 -0300

    🏷️ add and update types

---
+++ src/types/RealEstate.d.ts
@@ -0,0 +1,11 @@
+type TRealEstate = {
+  id?: string;
+  name: string;
+  cnpj: string;
+  city?: string;
+  initialProperties?: string;
+  initialBrokers?: string;
+  properties?: number;
+  brokers?: number;
+  status?: string;
+};
