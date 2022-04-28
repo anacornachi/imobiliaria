0596bac0722f20301786af6172ea8ce578ccfb75
Helena Machado <helena.machado.hm@gmail.com>
2022-04-28 16:47:15 -0300

    :recycle: refactor team component

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,22 +1,23 @@
 import {Box, Flex, Text, Link as ChakraLink, Heading} from '@chakra-ui/react';
 import {BsGithub, BsLinkedin} from 'react-icons/bs';
-import ana from '@public/assets/images/ana.png';
-import helena from '@public/assets/images/helena.png';
-import ivan from '@public/assets/images/ivan.png';
-import nadia from '@public/assets/images/nadia.png';
-import sabrina from '@public/assets/images/sabrina.png';
 import Container from '@components/layout/Container';
 import Link from 'next/link';
 import Image from 'next/image';
+import {teamData} from './teamData';
 
 export default function Team() {
   return (
-    <>
-      <Container>
-        <Flex direction="column">
-          <Flex ml="40px" alignSelf={{lg: 'start'}}>
+    <Container>
+      <Flex direction="column">
+        {teamData.map((contributor) => (
+          <Flex key={contributor.name}>
             <Box>
-              <Image src={ana} alt="ana" width="200px" height="200px" />
+              <Image
+                src={contributor.image}
+                alt="ana"
+                width="200px"
+                height="200px"
+              />
             </Box>
             <Flex direction="column">
               <Heading
@@ -26,16 +27,15 @@ export default function Team() {
                 w="100%"
                 m="30px"
               >
-                Ana Cornachi
+                {contributor.name}
               </Heading>
-              <Text ml="30px">Front End </Text>
+              <Text ml="30px">{contributor.focusIn}</Text>
               <Text ml="30px" mt="10px">
-                React.js | Next.js | TypeScript | Node.js | Jest | Testing
-                Library
+                {contributor.technologies}
               </Text>
 
               <Flex width="50px" justify="space-between" ml="30px" mt="15px">
-                <Link href="https://github.com/anacornachi" passHref>
+                <Link href={contributor.github} passHref>
                   <ChakraLink
                     color="title"
                     _hover={{color: 'primaryBlue'}}
@@ -45,7 +45,7 @@ export default function Team() {
                     <BsGithub />
                   </ChakraLink>
                 </Link>
-                <Link href="https://www.linkedin.com/in/anacornachi/" passHref>
+                <Link href={contributor.linkedin} passHref>
                   <ChakraLink
                     color="title"
                     _hover={{color: 'primaryBlue'}}
@@ -58,186 +58,8 @@ export default function Team() {
               </Flex>
             </Flex>
           </Flex>
-          <Flex ml="40px" alignSelf={{lg: 'end'}}>
-            <Box>
-              <Image src={helena} alt="helena" width="200px" height="200px" />
-            </Box>
-            <Flex direction="column">
-              <Heading
-                as="h5"
-                fontSize={{base: '15px', md: '15px', lg: '20px'}}
-                top={{base: 5, md: 10}}
-                w="100%"
-                m="30px"
-              >
-                Helena Machado
-              </Heading>
-              <Text ml="30px">Back End </Text>
-              <Text ml="30px" mt="10px">
-                Node.js | Express | JavaScript | Nest.js | SQL
-              </Text>
-
-              <Flex width="50px" justify="space-between" ml="30px" mt="15px">
-                <Link href="https://github.com/helena-machado" passHref>
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsGithub />
-                  </ChakraLink>
-                </Link>
-                <Link
-                  href="https://www.linkedin.com/in/helenamachadop/"
-                  passHref
-                >
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsLinkedin />
-                  </ChakraLink>
-                </Link>
-              </Flex>
-            </Flex>
-          </Flex>
-          <Flex ml="40px" alignSelf={{lg: 'start'}}>
-            <Box>
-              <Image src={ivan} alt="ivan" width="200px" height="200px" />
-            </Box>
-            <Flex direction="column">
-              <Heading
-                as="h5"
-                fontSize={{base: '15px', md: '15px', lg: '20px'}}
-                top={{base: 5, md: 10}}
-                w="100%"
-                m="30px"
-              >
-                Ivan Geier
-              </Heading>
-              <Text ml="30px">Back End </Text>
-              <Text ml="30px" mt="10px">
-                Node.js | Nest.js | SQL | NoSQL | Express
-              </Text>
-
-              <Flex width="50px" justify="space-between" ml="30px" mt="15px">
-                <Link href="https://github.com/ivangeier" passHref>
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsGithub />
-                  </ChakraLink>
-                </Link>
-                <Link href="https://www.linkedin.com/in/ivangeier/" passHref>
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsLinkedin />
-                  </ChakraLink>
-                </Link>
-              </Flex>
-            </Flex>
-          </Flex>
-          <Flex ml="40px" alignSelf={{lg: 'end'}}>
-            <Box>
-              <Image src={nadia} alt="nadia" width="200px" height="200px" />
-            </Box>
-            <Flex direction="column">
-              <Heading
-                as="h5"
-                fontSize={{base: '15px', md: '15px', lg: '20px'}}
-                top={{base: 5, md: 10}}
-                w="100%"
-                m="30px"
-              >
-                Nádia Mizael
-              </Heading>
-              <Text ml="30px">Front End </Text>
-              <Text ml="30px" mt="10px">
-                JavaScript | TypeScript | React.js | HTML/CSS
-              </Text>
-
-              <Flex width="50px" justify="space-between" ml="30px" mt="15px">
-                <Link href="https://github.com/Nadiamizael" passHref>
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsGithub />
-                  </ChakraLink>
-                </Link>
-                <Link href="https://www.linkedin.com/in/nadiamizael/" passHref>
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsLinkedin />
-                  </ChakraLink>
-                </Link>
-              </Flex>
-            </Flex>
-          </Flex>
-          <Flex ml="40px" alignSelf={{lg: 'start'}}>
-            <Box>
-              <Image src={sabrina} alt="sabrina" width="200px" height="200px" />
-            </Box>
-            <Flex direction="column">
-              <Heading
-                as="h5"
-                fontSize={{base: '15px', md: '15px', lg: '20px'}}
-                top={{base: 5, md: 10}}
-                w="100%"
-                m="30px"
-              >
-                Sabrina Couto
-              </Heading>
-              <Text ml="30px">Full Stack </Text>
-              <Text ml="30px" mt="10px">
-                HTML | CSS | JavaScript | Bootstrap | MySQL | Node.js
-              </Text>
-
-              <Flex width="50px" justify="space-between" ml="30px" mt="15px">
-                <Link href="https://github.com/sabrinacouto" passHref>
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsGithub />
-                  </ChakraLink>
-                </Link>
-                <Link
-                  href="https://www.linkedin.com/in/sabrinacoutolima/"
-                  passHref
-                >
-                  <ChakraLink
-                    color="title"
-                    _hover={{color: 'primaryBlue'}}
-                    _focus={{boxShadow: 'none'}}
-                    target="_blank"
-                  >
-                    <BsLinkedin />
-                  </ChakraLink>
-                </Link>
-              </Flex>
-            </Flex>
-          </Flex>
-        </Flex>
-      </Container>
-    </>
+        ))}
+      </Flex>
+    </Container>
   );
 }
---
+++ src/components/Team/teamData.ts
@@ -0,0 +1,49 @@
+import ana from '@public/assets/images/ana.png';
+import helena from '@public/assets/images/helena.png';
+import ivan from '@public/assets/images/ivan.png';
+import nadia from '@public/assets/images/nadia.png';
+import sabrina from '@public/assets/images/sabrina.png';
+
+export const teamData = [
+  {
+    image: ana,
+    name: 'Ana Cornachi',
+    focusIn: 'Front End',
+    technologies:
+      'React.js | Next.js | TypeScript | Node.js | Jest | Testing Library',
+    linkedin: 'https://github.com/anacornachi',
+    github: 'https://github.com/anacornachi',
+  },
+  {
+    image: helena,
+    name: 'Helena Machado',
+    focusIn: 'Back End',
+    technologies: '  Node.js | Express | JavaScript | Nest.js | SQL',
+    linkedin: 'https://www.linkedin.com/in/helenamachadop',
+    github: 'https://github.com/helena-machado',
+  },
+  {
+    image: ivan,
+    name: 'Ivan Geier',
+    focusIn: 'Back End',
+    technologies: 'Node.js | Nest.js | SQL | NoSQL | Express',
+    linkedin: 'https://www.linkedin.com/in/ivangeier',
+    github: 'https://github.com/ivangeier',
+  },
+  {
+    image: nadia,
+    name: 'Nádia Mizael',
+    focusIn: 'Front End',
+    technologies: 'JavaScript | TypeScript | React.js | HTML/CSS',
+    linkedin: 'https://www.linkedin.com/in/nadiamizael',
+    github: 'https://github.com/Nadiamizael',
+  },
+  {
+    image: sabrina,
+    name: 'Sabrina Couto',
+    focusIn: 'Front End',
+    technologies: 'HTML | CSS | JavaScript | Bootstrap | MySQL | Node.js',
+    linkedin: 'https://www.linkedin.com/in/sabrinacoutolima',
+    github: 'https://github.com/sabrinacouto',
+  },
+];
